d12a2af84943edf93c9475883a6c261130781ff9
Daddyjohn63 <john@webandprosper.co.uk>
2023-11-05 15:02:25 +0000

    5 setup routes pre auth0

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div />;
+  return <h1>app</h1>;
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,3 @@
+export default function TokenTopup() {
+  return <div>this is the token topup page</div>;
+}
